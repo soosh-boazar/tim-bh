8beefae6451eeb2b420cf0da70629c39b23ba121
soosh-boazar <qq0598995@gmail.com>
2025-05-11 12:41:14 +0330

    Add files via upload

--- sms.py
+++ sms.py
@@ -1,10 +1,10 @@
 import requests
 import time
 
-Enter = input("enter number")
+
 url = "https://mc.yandex.ru/watch/94212660/1?page-url=https%3A%2F%2Fapp.snapp.taxi%2Flogin&c"
 while True:
     time.sleep(3)
-    number = {"cellphone":"+98"+Enter}
+    number = {"cellphone":"+989133894972"}
     sms = requests.post(url,data=number)
     print(sms.status_code)
